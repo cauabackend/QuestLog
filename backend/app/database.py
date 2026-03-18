fcc40d9e0dce107bb8e11171f47fd87b8006d8ee
cauabackend <cauabackend@gmail.com>
2026-03-18 20:52:34 -0300

    segundo commmit - continuação do backend do projeto

---
+++ backend/app/database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+from app.config import settings
+
+engine = create_engine(
+    settings.DATABASE_URL,
+    connect_args={"check_same_thread": False},
+)
+
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+
+class Base(DeclarativeBase):
+    pass
